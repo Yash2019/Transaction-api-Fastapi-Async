fd8d4e31718293a49702952ddeb2de5b32203694
Yash2019 <rogerplay4639@gmail.com>
2026-02-06 19:22:21 +0530

    fixed bugs

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ from db import creat_table
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def on_startup() -> None:
+    await creat_table()
+
 app.include_router(
-    router,
-    
+    router
 )
--- project/models.py
+++ project/models.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 from typing import List
-from sqlalchemy.orm import Mapped, mapped_column, Relationship
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from db import Base
-from uuid import UUID
+#from uuid import UUID
 from sqlalchemy import func, ForeignKey
 from decimal import Decimal
 
@@ -18,11 +18,11 @@ class Account(Base):
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
 
     #Relationships
-    sent_transaction: Mapped[List['Transaction']] = Relationship(
+    sent_transaction: Mapped[List['Transaction']] = relationship(
         back_populates='sender',
         foreign_keys='[Transaction.sender_id]'
     )
-    recieved_transaction: Mapped[List['Transaction']] = Relationship(
+    recieved_transaction: Mapped[List['Transaction']] = relationship(
         back_populates='reciever',
         foreign_keys='[Transaction.reciever_id]'
     )
@@ -32,18 +32,18 @@ class Transaction(Base):
     '''
     Do not store any transaction but the information about the processing
     '''
-    __tablename__ = 'transaction'
+    __tablename__ = 'transactions'
     id: Mapped[int] = mapped_column(primary_key=True)
     sender_id: Mapped[int] = mapped_column(ForeignKey('accounts.id'))
     reciever_id: Mapped[int] = mapped_column(ForeignKey('accounts.id'))
     timestamp: Mapped[datetime] = mapped_column(server_default=func.now())
 
     #relationship
-    sender: Mapped['Account'] = Relationship(
+    sender: Mapped['Account'] = relationship(
         back_populates='sent_transaction',
         foreign_keys=[sender_id]
     )
-    reciever: Mapped['Account'] = Relationship(
+    reciever: Mapped['Account'] = relationship(
         back_populates='recieved_transaction',
         foreign_keys=[reciever_id]
     )
